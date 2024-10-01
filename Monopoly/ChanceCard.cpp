0c902bcba746a175b21aa6628ecca004eabbb452
ShoamIv <155763643+ShoamIv@users.noreply.github.com>
2024-10-01 20:32:52 +0300

    Delete Monopoly directory

--- Monopoly/ChanceCard.cpp
+++
@@ -1,9 +0,0 @@
-
-#include "ChanceCard.hpp"
-
-ChanceCard::ChanceCard(ChanceCard::Effect effect,int amount) : effect(effect) , amount(amount){}
-
-ChanceCard ChanceCard::DrawCard(Player &player) {
-    return {ChanceCard::Effect::AdvanceToStCharlesPlace, 0};
-}
-
--- Monopoly/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <sstream>
-#include "GameFlow.hpp"
-#include "Board.hpp"
-int main() {
-    Board * board = Board::getBoard();
-}
